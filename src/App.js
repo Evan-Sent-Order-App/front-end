0a0372a317e0795abdea04620d849a06c6b1c68a
iamleahb <leah.ball@gmail.com>
2022-04-20 15:48:45 -0500

    deleted firebase

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ import { Order } from "./Order/Order";
 import { useOpenFood } from "./Hooks/useOpenFood";
 import { useOrders } from "./Hooks/useOrders";
 import { useTitle } from "./Hooks/useTitle";
-import { AuthProvider } from "./components/AuthProvider";
-import { FirebaseProvider } from "./components/FirebaseProvider";
-import { RestOfTheApp } from "./Components/RestOfTheApp";
 
-// const auth = window.firebase.auth();
-// const provider = new window.firebase.auth.GoogleAuthProvider();
-// auth.signInWithPopup(provider)
 
 function App() {
   const openFood = useOpenFood();
@@ -23,17 +17,13 @@ function App() {
 
   return (
     <>
-      <FirebaseProvider>
-        <AuthProvider>
           <GlobalStyle />
-          <RestOfTheApp />
           <FoodDialog {...openFood} {...orders} />
           <Navbar />
           <Order {...orders} {...openFood} />
           <Banner />
           <Menu {...openFood} />
-        </AuthProvider>
-      </FirebaseProvider>
+  
     </>
   );
 }
